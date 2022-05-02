d0282754b0677bbc01b091b729f0b4f8e8bc23c8
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2022-05-02 00:55:32 -0400

    Add redis as caching service support

--- src/WebApplication1/Program.cs
+++ src/WebApplication1/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("redis");
+});
 
 var app = builder.Build();
 
@@ -21,17 +28,30 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async (IDistributedCache cache) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var weather = await cache.GetStringAsync("weather");
+
+    if (weather is null)
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateTime.Now.AddDays(index),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+
+        weather = JsonSerializer.Serialize(forecast);
+
+        await cache.SetStringAsync("weather", weather, new DistributedCacheEntryOptions()
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5)
+        });
+    }
+
+    return weather;
 })
 .WithName("GetWeatherForecast");
 
